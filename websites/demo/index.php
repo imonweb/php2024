da0d298d44fbd8f9d74b22b9b9dd93bc65bebc69
Imon <contact@imonweb.co.uk>
2024-04-02 17:46:16 +0100

    filter

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -21,27 +21,54 @@
       [
         'name' => 'Do Androids Dream of Electric Sheep',
         'author' => 'Philip K. Dick',
+        'releaseYear' => 1968,
         'purchaseUrl' => 'https://example.com'
       ],
       [
         'name' => 'Project Hail Mary',
         'author' => 'Andy Weir',
+        'releaseYear' => 2021,
+        'purchaseUrl' => 'https://example.com'
+      ],
+      [
+        'name' => 'The Martin',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2011,
         'purchaseUrl' => 'https://example.com'
       ],
 
     ];
 
+    function filterByAuthor($books, $author) {
+      // 
+      $filteredBooks = [];
+
+      foreach($books as $book){
+        if($book['author'] === $author){
+          $filteredBooks[] = $book;
+        }
+      }
+      return $filteredBooks;
+    }
+ 
+
   ?>
 
   <ul>
-    <?php foreach($books as $book) : ?>
+    <?php foreach(filterByAuthor($books, 'Andy Weir') as $book) : ?>
+      
         <li>
-          <a href="<?= $book['purchaseUrl']; ?>">
-            <?= $book['name']; ?>
+          <a href="<?= $book['purchaseUrl'] ?>">
+            <?= $book['name']; ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
           </a>
         </li>
+   
     <?php endforeach; ?>
   
   </ul>
+
+  <p>
+    <?= filterByAuthor($books); ?>
+  </p>
 </body>
 </html>
