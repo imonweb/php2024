aa4f48cd02e73dd27608c0af270a819f42e46c22
Imon <contact@imonweb.co.uk>
2024-04-04 23:35:28 +0100

    lambda functions completed

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -14,50 +14,7 @@
   </style>
 </head>
 <body>
-  <h1>Recommended Books for you</h1>
-
-  <?php 
-    $books = [
-      [
-        'name' => 'Do Androids Dream of Electric Sheep',
-        'author' => 'Philip K. Dick',
-        'releaseYear' => 1968,
-        'purchaseUrl' => 'https://example.com'
-      ],
-      [
-        'name' => 'Project Hail Mary',
-        'author' => 'Andy Weir',
-        'releaseYear' => 2021,
-        'purchaseUrl' => 'https://example.com'
-      ],
-      [
-        'name' => 'The Martin',
-        'author' => 'Andy Weir',
-        'releaseYear' => 2011,
-        'purchaseUrl' => 'https://example.com'
-      ],
-
-    ];
-
-    function filter($items, $fn) {
-      // 
-      $filteredItems = [];
-
-      foreach($items as $item){
-        if($fn($item)){
-          $filteredItems[] = $item;
-        }
-      }
-      return $filteredItems;
-    };
-
-    $filteredBooks = array_filter($books, function($book){
-      return $book['releaseYear'] < 2000;
-      // return $book['author'] === 'Andy Weir';
-    });
- 
-
-  ?>
+  
 
   <ul>
     <?php foreach($filteredBooks as $book) : ?>
